acee45322bf707d747c1539d805cc2ee8be4e419
Vitaly Litvak <vitavaque@gmail.com>
2013-12-24 22:41:36 +0400

    Added test for folder with mapped sub folders

--- src/test/java/su/litvak/justdlna/dlna/FolderNodeTest.java
+++ src/test/java/su/litvak/justdlna/dlna/FolderNodeTest.java
@@ -1,10 +1,12 @@
 package su.litvak.justdlna.dlna;
 
 import org.junit.Test;
+import su.litvak.justdlna.model.ContainerNode;
 import su.litvak.justdlna.model.FolderNode;
 import su.litvak.justdlna.model.VideoFormat;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -22,5 +24,22 @@ public class FolderNodeTest extends AbstractTest {
         assertTrue(subFolder.getItems().isEmpty());
         assertEquals(0, subSubFolder.getContainers().size());
         assertEquals(1, subSubFolder.getItems().size());
+    }
+
+    @Test
+    public void testFolderWithMappedSubFolders() throws IOException {
+        FolderNode<VideoFormat> folder1 = mockDir("Video 1", VideoFormat.class);
+        FolderNode<VideoFormat> folder2 = mockDir("Video 2", VideoFormat.class);
+        FolderNode<VideoFormat> folder3 = mockDir("Video 3", VideoFormat.class);
+
+        mockFile("video", VideoFormat.WMV, mockDir("Sub video 1", folder1));
+        mockFile("video", VideoFormat.MPEG, folder1);
+        mockFile("video", VideoFormat.MKV, folder2);
+        mockFile("video", VideoFormat.FLV, folder3);
+
+        folder1.setContainers(Arrays.<ContainerNode>asList(folder2, folder3));
+
+        assertEquals(3, folder1.getContainers().size());
+        assertEquals(1, folder1.getItems().size());
     }
 }
